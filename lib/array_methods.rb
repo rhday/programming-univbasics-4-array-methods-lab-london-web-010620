136dd587ff9239eb1c609b0411e52e3e53164193
Ruari Day <ruariday@gmail.com>
2019-11-25 15:37:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,11 +5,11 @@ end
 def using_sort(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.sort!
-
 end
 
 def using_reverse(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.reverse!
 end
 
 def using_first(array)
